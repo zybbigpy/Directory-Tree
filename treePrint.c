fe3670fbc0f0e47c9420bb5e886131790e1df804
zybbigpy <miaowangqian@icloud.com>
2019-03-08 15:37:25 +0800

    add result

--- treePrint.c
+++ treePrint.c
@@ -3,12 +3,29 @@
 
 #define MAX_TREE_DEPTH 10
 
+/* for test use */
+#define new_tree_node(name) \
+  TreeNode *name = (TreeNode *)malloc(sizeof(TreeNode))
+
+#define fill_tree_info(name, left_ptr, right_ptr, level_, ch_) \
+  name->left_brother = left_ptr;                               \
+  name->right_child = right_ptr;                               \
+  name->level = level_;                                        \
+  name->ch = ch_
+
 typedef struct TreeNode {
   struct TreeNode *left_brother;
   struct TreeNode *right_child;
   char ch;
   int level;
 } TreeNode;
+
+void destroy_tree(TreeNode *root) {
+  if (root == NULL) return;
+  destroy_tree(root->left_brother);
+  destroy_tree(root->right_child);
+  free(root);
+}
 
 int rec[MAX_TREE_DEPTH] = {0};  // record the level information
 void tree_print(TreeNode *root) {
@@ -34,75 +51,35 @@ void tree_print(TreeNode *root) {
   }
 }
 
-int main() {
-  TreeNode *R = (TreeNode *)malloc(sizeof(TreeNode));  // root
-  TreeNode *a = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *b = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *c = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *d = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *e = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *f = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *g = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *h = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *i = (TreeNode *)malloc(sizeof(TreeNode));
-  TreeNode *j = (TreeNode *)malloc(sizeof(TreeNode));
+void test() {
+  new_tree_node(R);  // root node
+  new_tree_node(a);
+  new_tree_node(b);
+  new_tree_node(c);
+  new_tree_node(d);
+  new_tree_node(e);
+  new_tree_node(f);
+  new_tree_node(g);
+  new_tree_node(h);
+  new_tree_node(i);
+  new_tree_node(j);
 
-  R->ch = '0';
-  R->left_brother = NULL;
-  R->right_child = a;
-  R->level = 0;
-
-  a->ch = 'A';
-  a->left_brother = b;
-  a->right_child = c;
-  a->level = 1;
-
-  b->ch = 'B';
-  b->left_brother = d;
-  b->right_child = e;
-  b->level = 1;
-
-  c->ch = 'C';
-  c->left_brother = f;
-  c->right_child = g;
-  c->level = 2;
-
-  d->ch = 'D';
-  d->left_brother = NULL;
-  d->right_child = j;
-  d->level = 1;
-
-  e->ch = 'E';
-  e->left_brother = h;
-  e->right_child = NULL;
-  e->level = 2;
-
-  f->ch = 'F';
-  f->left_brother = NULL;
-  f->right_child = i;
-  f->level = 2;
-
-  g->ch = 'G';
-  g->left_brother = NULL;
-  g->right_child = NULL;
-  g->level = 3;
-
-  h->ch = 'H';
-  h->left_brother = NULL;
-  h->right_child = NULL;
-  h->level = 2;
-
-  i->ch = 'I';
-  i->left_brother = NULL;
-  i->right_child = NULL;
-  i->level = 3;
-
-  j->ch = 'J';
-  j->left_brother = NULL;
-  j->right_child = NULL;
-  j->level = 2;
-
+  fill_tree_info(R, NULL, a, 0, '0');
+  fill_tree_info(a, b, c, 1, 'A');
+  fill_tree_info(b, d, e, 1, 'B');
+  fill_tree_info(c, f, g, 2, 'C');
+  fill_tree_info(d, NULL, j, 1, 'D');
+  fill_tree_info(e, h, NULL, 2, 'E');
+  fill_tree_info(f, NULL, i, 2, 'F');
+  fill_tree_info(g, NULL, NULL, 3, 'G');
+  fill_tree_info(h, NULL, NULL, 2, 'H');
+  fill_tree_info(i, NULL, NULL, 3, 'I');
+  fill_tree_info(j, NULL, NULL, 2, 'J');
   tree_print(R);
-  // printf("hello world\n");
+  destroy_tree(R);
+}
+
+int main() {
+  test();
   return 0;
 }
